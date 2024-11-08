cc81a3f6c110c177ff8ba2066a284ed894804c10
Austin Song <austinhssong@gmail.com>
2024-11-08 14:56:39 -0500

    Setup backend project structure

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Welcome to SnipHub API');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
