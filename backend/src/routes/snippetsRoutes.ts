998cf83760888e2e3ef478883f5a3d2a0607e72a
Austin Song <austinhssong@gmail.com>
2024-11-15 14:22:41 -0500

    Add controller functions to access snippets table from backend

--- backend/src/routes/snippetsRoutes.ts
+++ backend/src/routes/snippetsRoutes.ts
@@ -3,6 +3,7 @@ import { getAllSnippets, getSnippetById, createSnippet, updateSnippet, deleteSni
 
 const router = express.Router();
 
+//CRUD routes
 router.get('/', getAllSnippets);
 router.get('/:id', getSnippetById);
 router.post('/', createSnippet);
